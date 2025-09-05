4fbbd54ff14db16a99911e49dfe66ab70e5a3104
Saidbek Saidakbarov <saidbek2504@gmail.com>
2025-09-05 22:12:09 +0500

    completed project

--- app.py
+++ app.py
@@ -1,63 +1,124 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, g
 from extension import db
 import os
+from functools import wraps
+# from flask_admin import Admin
+# from flask_admin.contrib.sqla import ModelView
 
+# Flask app
 app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app.secret_key = 'n9$2@!Df6wE^zXoB7rL0#QsA3!vUz1gT'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# инициализируем базу
 db.init_app(app)
 
-# импорт моделей только после db.init_app
+# import models
 from models import Oquvchi, Course, Category, Eduverse, VideoContent, Product
+
+# from flask_admin.contrib.sqla import ModelView
+
+# class EduverseAdmin(ModelView):
+#     form_columns = ['name', 'description', 'category', 'video']  # укажем поля
+
+# # Flask-Admin
+# admin = Admin(app, name='My Admin Panel', template_mode='bootstrap4')
+# admin.add_view(ModelView(Oquvchi, db.session))
+# admin.add_view(ModelView(Course, db.session))
+# admin.add_view(ModelView(Category, db.session))
+# admin.add_view(ModelView(VideoContent, db.session))
+# admin.add_view(EduverseAdmin(Eduverse, db.session))  # кастомная админка
+# admin.add_view(ModelView(Product, db.session))
+
+
+
+# ---- Авторизация ----
+def login_required(view_fn):
+    @wraps(view_fn)
+    def wrapper(*args, **kwargs):
+        if 'user_id' not in session:
+            return redirect(url_for('login'))
+        return view_fn(*args, **kwargs)
+    return wrapper
+
+
+@app.before_request
+def load_current_user():
+    g.current_user = None
+    uid = session.get('user_id')
+    if uid:
+        g.current_user = Oquvchi.query.get(uid)
 
 
 @app.route('/')
+@login_required
 def home():
-
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     err = None
     if request.method == 'POST':
-        modme_id = request.form.get("modme_id")
-        parol = request.form.get("parol")
+        username = request.form.get("modme_id")   # form поле
+        password = request.form.get("parol")
 
-        # Oddiy tekshiruv
-        if modme_id == "12345" and parol == "123456":
-            return render_template('index.html')
+        user = Oquvchi.query.filter_by(username=username, password=password).first()
+
+        if user:
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
         else:
-            err = "Parol noto'g'ri!"
+            err = "Parol yoki login noto‘g‘ri!"
 
     return render_template('login.html', err=err)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+
+# ---- Остальные страницы ----
 @app.route('/courses')
+@login_required
 def courses():
-    # courses = Course.query.all()
-    return render_template('courses.html')
+    user = g.current_user
+    user_courses = user.courses if user else []
+    return render_template('courses.html', courses=user_courses)
+
 
 @app.route('/eduverse')
+@login_required
 def eduverse():
-    # eduverses = Eduverse.query.all()    
-    return render_template('eduverse.html')
+    eduverses = Eduverse.query.all()
+    categories = Category.query.all()
+    return render_template('eduverse.html', eduverses=eduverses, categories=categories)
+
 
 @app.route('/marscode')
+@login_required
 def marscode():
     return render_template('marscode.html')
 
+
 @app.route('/blog')
+@login_required
 def blog():
     return render_template('blog.html')
 
+
 @app.route('/shop')
+@login_required
 def shop():
-    return render_template('shop.html')
+    products = Product.query.all()
+    return render_template('shop.html', products=products)
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # создаём таблицы
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from extension import db
 from datetime import datetime
 
+# таблица-связка
+oquvchi_courses = db.Table(
+    'oquvchi_courses',
+    db.Column('oquvchi_id', db.Integer, db.ForeignKey('oquvchi.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+)
+
 class Oquvchi(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
@@ -8,12 +15,17 @@ class Oquvchi(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     coins = db.Column(db.Integer, default=0)
     quvvat = db.Column(db.Integer, default=100)
-    course = db.Column(db.ForeignKey('course.id'), nullable=True)
+
+    courses = db.relationship('Course', secondary=oquvchi_courses, backref='students')
 
     def __repr__(self):
         return f'<User {self.username}>'
+
     
-    
+    def my_courses(self):
+        return self.courses
+
+        
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -29,29 +41,41 @@ class Category(db.Model):
 
     def __repr__(self):
         return f'<Category {self.name}>'
+
+
+class VideoContent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    url = db.Column(db.String(300), nullable=False)
+
+    @property
+    def youtube_id(self):
+        if "youtu.be/" in self.url:
+            return self.url.split("/")[-1].split("?")[0]
+        elif "watch?v=" in self.url:
+            return self.url.split("v=")[-1].split("&")[0]
+        elif "embed/" in self.url:
+            return self.url.split("embed/")[-1].split("?")[0]
+        return self.url
+
+    def __repr__(self):
+        return f'<VideoContent {self.title}>'
+
     
 class Eduverse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    category = db.Column(db.ForeignKey('category.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey("video_content.id"))
 
-    def __repr__(self):
-        return f'<Eduverse {self.name}>'
-    
-class VideoContent(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    url = db.Column(db.String(300), nullable=False)
-    eduverse = db.Column(db.ForeignKey('eduverse.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<VideoContent {self.title}>'
+    category = db.relationship('Category', backref='eduverses')
+    video = db.relationship('VideoContent', backref='eduverses')
     
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     photo_url = db.Column(db.String(300), nullable=True)
 
     def __repr__(self):
